793897453b9d9ea0af8dfc97fbcb228813cf305c
Richelle <richelle.ak@gmail.com>
2023-08-28 13:45:27 -0700

    add list view and uuid to each expense

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -9,6 +10,18 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Costco',
+        amount: 402.95,
+        date: DateTime.now(),
+        category: Category.food),
+     Expense(
+        title: 'Alaska Airlines',
+        amount: 407.95,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
